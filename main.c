140be99633492fa89589ca89a0feda52b54eb1ca
Peter <peter.kaae.thorhauge@gmail.com>
2024-11-12 15:29:52 +0100

    Added flow array function

--- main.c
+++ main.c
@@ -3,11 +3,11 @@
 
 #define ARRAY_SIZE 2
 
-// The output array shoud be dataArray[dateTime][frequency]
-int (*array_from_file(char *filePath))[ARRAY_SIZE];
+// The output array shoud be dataArray[dateTime][rotations]
+int (*array_from_file(char *filePath, int *size))[ARRAY_SIZE];
 
 // The flow can be calculated using calculus Q=dV/dt out array shoud be flowArray[dateTime][flow]
-double *flow_array(int *dataArray);
+double (*flow_array(int (*dataArray)[ARRAY_SIZE], int size))[ARRAY_SIZE];
 
 // This function takes the avage flow in a given time
 double avage_flow(int startDateTime, int endDateTime, double *flowArray);
@@ -27,20 +27,53 @@ void draw_graph(double *array);
 int main(void) {
     // Declare a two dimensional array, without a set length.
     int (*array)[ARRAY_SIZE];
+    double (*flowArray)[ARRAY_SIZE];
+    int size;
 
-    array = array_from_file("data.txt");
+    array = array_from_file("data.txt", &size);
 
-    // Prints time and frequency.
-    for (int i = 0; i < 5; i++) {
+    flowArray = flow_array(array, size);
+
+    // Prints time and rotations.
+    for (int i = 0; i < size; i++) {
         printf("%d %d\n", array[i][0], array[i][1]);
     }
+
+    for (int i = 0; i < size; i++) {
+        printf("%f %f\n", flowArray[i][0], flowArray[i][1]);
+    }
+
+    free(array);
 
     return 0;
 }
 
+double (*flow_array(int (*dataArray)[ARRAY_SIZE], int size))[ARRAY_SIZE] {
+    // We have to double the size of the malloc because we will have two doubles in each index.
+    double (*flowArray)[ARRAY_SIZE] = malloc(sizeof(double) * size * ARRAY_SIZE);
+    double V = 0.1; // 0.1 L CHANGE ME
+    double deltaTime;
+
+    // Validate the initialization of the array.
+    if (flowArray == NULL) {
+        printf("Error in array\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < size; i++) {
+        flowArray[i][0] = (double)dataArray[i][0];
+
+        // Asumes the time intervals do not change.
+        deltaTime = (double)dataArray[1][0]-(double)dataArray[0][0];
+        flowArray[i][1] = (double)dataArray[i][1]*V/deltaTime;
+    }
+
+    return flowArray;
+}
+
 // This function reads the data from a file and stores it in a two dimensional array.
-int (*array_from_file(char *filePath))[ARRAY_SIZE] {
-    int time, frequency;
+int (*array_from_file(char *filePath, int *size))[ARRAY_SIZE] {
+    int time, rotations;
     int lines = 0;
     char ch;
 
@@ -68,6 +101,10 @@ int (*array_from_file(char *filePath))[ARRAY_SIZE] {
             lines++;
         }
     }
+
+    // Returns the size of the array as pointer.
+    *size = lines;
+
     // We have to double the size of the malloc because we will have two integers in each index.
     int (*array)[ARRAY_SIZE] = malloc(sizeof(int) * lines * ARRAY_SIZE);
 
@@ -77,7 +114,7 @@ int (*array_from_file(char *filePath))[ARRAY_SIZE] {
         exit(EXIT_FAILURE);
     }
 
-    // Reset the file pointer. Resets the current position of the pointer to the start of the file
+    // Resets the current position of the pointer to the start of the file.
     rewind(file);
     
     // Read the file and store the values in the array.
@@ -85,9 +122,9 @@ int (*array_from_file(char *filePath))[ARRAY_SIZE] {
         // Read the line from the file and sizeof(line) is the size of the buffer.
         fgets(line, sizeof(line), file);
         // sscanf is used to read formatted input from a string.
-        sscanf(line, "%d %d", &time, &frequency);
+        sscanf(line, "%d %d", &time, &rotations);
         array[i][0] = time;
-        array[i][1] = frequency;
+        array[i][1] = rotations;
     }
 
     fclose(file);
