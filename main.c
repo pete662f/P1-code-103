5c0b1e40b6bcd46419b10db9db847f6eb794741b
mehmet <mehmetaa@outlook.dk>
2024-11-14 14:28:00 +0100

    edits

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ void water_level_graph();
 void water_level_statistics();
 void flow_graph();
 void set_water_level_alarm();
-void main_menu()
+void main_menu();
 void sensor_menu();
 
 int main(void) {
@@ -15,25 +15,44 @@ int main(void) {
     return 0;
 }
 
-
-void water_level_graph() {
-    printf("       ");
-    // Code here
-}
-
-void water_level_statistics() {
-    printf("       ");
-    // Code here
-}
-
-void flow_graph() {
-    printf("       ");
-    // Code here
-}
-
-void set_water_level_alarm() {
-    printf("       ");
-    // Code here
+void main_menu() {
+    int choice;
+    while (1) {
+        printf("\n--- Main menu ---\n");
+        printf("1: Water level graph\n");
+        printf("2: Water level Statistics\n");
+        printf("3: Flow graph\n");
+        printf("4: Set water level alarm\n");
+        printf("5: Sensor menu\n");
+        printf("0: finish\n");
+        printf("Choose where you want to go:");
+        scanf("%d", &choice);
+    
+    
+        switch (choice) {
+            case 1:
+                water_level_graph();
+                break;
+            case 2:
+                water_level_statistics();
+                break;
+            case 3:
+                flow_graph();
+                break;
+            case 4:
+                set_water_level_alarm();
+                break;
+            case 5:
+                sensor_menu();
+                break;
+            case 0:
+                printf("Finishing the program");   // hvis man skriver 0
+                break;
+            default:
+                printf("Error: No choice!");
+        
+        }
+    }
 }
 
 void sensor_menu() {
@@ -44,7 +63,7 @@ void sensor_menu() {
     printf("1. Sensor 1\n");
     printf("2. Sensor 2\n");
     printf("3. Sensor 3\n");
-    printf("Choose sensor");
+    printf("Choose sensor: ");
     scanf("%d", &sensor_choice);
 
     switch (sensor_choice) {
@@ -65,40 +84,27 @@ void sensor_menu() {
     }
 }
 
-void main_menu() {
-    int choice;
-    while (1) {
-        printf("\n--- Main menu ---\n");
-        printf("1: Water level graph\n");
-        printf("2: Water level Statistics\n");
-        printf("3: Flow graph\n");
-        printf("4: Set water level alarm\n");
-        printf("5: Sensor menu\n");
-        printf("0: finish\n");
-        printf("Choose where you want to go\n");
-        scanf("%d", &choice);
-    }
-    
-    switch (choice) {
-        case 1:
-            water_level_graph();
-            break;
-        case 2:
-            water_level_statistics();
-            break;
-        case 3:
-            flow_graph();
-            break;
-        case 4:
-            set_water_level_alarm();
-            break;
-        case 5:
-            sensor_menu();
-            break;
-        case 0:
-            printf("Finishing the program");   // hvis man skriver 0
-            break;
-        default:
-            printf("Error: No choice!");
-    }
+void water_level_graph() {
+    sensor_menu();
+    printf("       ");
+    // Code here
 }
+
+void water_level_statistics() {
+    sensor_menu();
+    printf("       ");
+    // Code here
+}
+
+void flow_graph() {
+    sensor_menu();
+    printf("       ");
+    // Code here
+}
+
+void set_water_level_alarm() {
+    sensor_menu();
+    printf("       ");
+    // Code here
+}
+
