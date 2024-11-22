ba161d5776914bc73f0124b7a2d310786165646d
Peter <peter.kaae.thorhauge@gmail.com>
2024-11-22 12:18:54 +0100

    Improved input validation

--- main.c
+++ main.c
@@ -3,11 +3,11 @@
 #include <strings.h>
 #include "functions.h"
 
-void water_level_graph();
-void water_level_statistics();
-void flow_graph();
-void set_water_level_alarm();
-void data_menu();
+void water_level_graph(int sensorChoice);
+void water_level_statistics(int sensorChoice);
+void flow_graph(int sensorChoice);
+void set_water_level_alarm(int sensorChoice);
+void data_menu(int sensorChoice);
 void sensor_menu();
 
 int main(void) {
@@ -27,36 +27,40 @@ void sensor_menu() {
     numberOfSensors = number_of_sensors("./data/");
     sensor *sensor = parth_of_sensors("./data/");
 
-    printf("\n--- Sensor Menu ---\n");
-    printf("0. All sensors\n");
+    do {
+        printf("\n--- Sensor Menu ---\n");
+        printf("0. All sensors\n");
 
-    for (int i = 0; i < numberOfSensors; i++) {
-        char *pch;
-        // Pch removes file exstention (.txt) on sensor menu
-        pch = strtok(sensor[i].name,".");
-        printf("%d. %s\n", i + 1, pch);
-    }
+        for (int i = 0; i < numberOfSensors; i++) {
+            char *pch;
+            // Pch removes file exstention (.txt) on sensor menu
+            pch = strtok(sensor[i].name,".");
+            printf("%d. %s\n", i + 1, pch);
+        }
 
+        
+
+        printf("Choose sensor: ");
+        isValid = scanf(" %d", &choice);
+
+        // Removes the input buffer
+        while (getchar() != '\n');    
+
+        if (choice > numberOfSensors || choice < 0 || !isValid) {
+            printf("\n\x1B[31mInvalid sensor choice!\x1B[0m\n");
+        }
+    } while (choice > numberOfSensors || choice < 0 || !isValid);
+    
     free(sensor);
 
-    printf("Choose sensor: ");
-    isValid = scanf(" %d", &choice);
-
-    // Removes the input buffer
-    while (getchar() != '\n');    
-
-    if (choice > numberOfSensors || choice < 0 || !isValid) {
-        printf("Invalid sensor choice!\n");
-        exit(EXIT_FAILURE);
-    }
-    
     data_menu(choice);
 }
 
 void data_menu(int sensorChoice) {
     int choice;
+    int isValid;
     
-    while (1) {
+    do {
         printf("\n--- Data Menu ---\n");
         printf("1: Water level graph\n");
         printf("2: Water level Statistics\n");
@@ -64,7 +68,7 @@ void data_menu(int sensorChoice) {
         printf("4: Set water level alarm\n");
         printf("0: Exit the program\n");
         printf("Choose where you want to go: ");
-        scanf(" %d", &choice);
+        isValid = scanf(" %d", &choice);
 
         // Removes the input buffer
         while (getchar() != '\n');    
@@ -87,9 +91,9 @@ void data_menu(int sensorChoice) {
                 exit(EXIT_SUCCESS);
                 break;
             default:
-                printf("Error: No choice!\n");
+                printf("\n\x1B[31mInvalid choice!\x1B[0m\n");
         }
-    }
+    } while (choice < 0 || choice > 4 || !isValid);
 }
 
 void water_level_graph(int sensorChoice) {
