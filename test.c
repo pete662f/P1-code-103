4530bcfad54536fbdb2b9316020be66fe2e102d5
Peter Kaae Thorhauge <peter.kaae.thorhauge@gmail.com>
2024-12-14 20:15:41 +0100

    Add minFlowTest and maxFlowTest to enhance flow analysis coverage

--- test.c
+++ test.c
@@ -1,20 +1,72 @@
 #include "functions.h"
 #include "CuTest.h"
 
-#define LEN 3
+enum minMaxBit {
+    MIN,
+    MAX
+};
 
 void averageFlowTest(CuTest* tc)
 {
-    flow flowArray[LEN] = {
+    double averageFlow = 0;
+    int len = 3;
+    int time = 0;
+
+    flow flowArray[3] = {
         {3600, 20},
         {3600*2, 30},
         {3600*3, 15}
     };
 
-    double averageFlow = average_flow(2, flowArray, LEN);
-
+    time = 2;
+    averageFlow = average_flow(time, flowArray, len);
     CuAssertDblEquals(tc, 25.0, averageFlow, 0.001);
-    
+}
+
+void minFlowTest(CuTest* tc)
+{
+    double minFlow = 0;
+    int len = 5;
+    int time = 0;
+
+    flow flowArray[5] = {
+        {3600, 20},
+        {3600*2, 30},
+        {3600*3, 15},
+        {3600*4, 35},
+        {3600*4, 10}
+    };
+
+    time = 3;
+    minFlow = min_max_flow(time, MIN, flowArray, len);
+    CuAssertDblEquals(tc, 15.0, minFlow, 0.001);
+
+    time = 5;
+    minFlow = min_max_flow(time, MIN, flowArray, len);
+    CuAssertDblEquals(tc, 10.0, minFlow, 0.001);
+}
+
+void maxFlowTest(CuTest* tc)
+{
+    double maxFlow = 0;
+    int len = 5;
+    int time = 0;
+
+    flow flowArray[5] = {
+        {3600, 20},
+        {3600*2, 30},
+        {3600*3, 15},
+        {3600*4, 35},
+        {3600*4, 10}
+    };
+
+    time = 3;
+    maxFlow = min_max_flow(time, MAX, flowArray, len);
+    CuAssertDblEquals(tc, 30.0, maxFlow, 0.001);
+
+    time = 5;
+    maxFlow = min_max_flow(time, MAX, flowArray, len);
+    CuAssertDblEquals(tc, 35.0, maxFlow, 0.001);
 }
 
 CuSuite* CuStringGetSuite(void)
@@ -23,6 +75,8 @@ CuSuite* CuStringGetSuite(void)
 	CuSuite* suite = CuSuiteNew();
 
 	SUITE_ADD_TEST(suite, averageFlowTest);
+    SUITE_ADD_TEST(suite, minFlowTest);
+    SUITE_ADD_TEST(suite, maxFlowTest);
 
 
 	return suite;
